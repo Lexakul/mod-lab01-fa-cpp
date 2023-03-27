5bcd2320b66136251be31382724da30fe3db77c2
LordlyThief <vostanzunileg1_2@mail.ru>
2023-03-27 23:45:25 +0300

    Lab1 Kuznetsova AA 381907-3

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,142 @@
-// Copyright 2022 UNN-IASR
+// Kuznetsova Angelina 381907-3
+#include <iostream>
+#include <cstring>
 #include "fun.h"
 
-unsigned int faStr1(const char *str) {
-    return 0;
+unsigned int faStr1(const char *str)
+{
+    int c = 0;
+    bool word = false;
+    bool number = false;
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if (isspace(str[i]))
+        {
+            if (word && !number)
+            {
+                c++;
+            }
+            word = false;
+            number = false;
+        }
+        else
+        {
+            if (!word)
+            {
+                word = true;
+            }
+            if (isdigit(str[i]))
+            {
+                number = true;
+            }
+        }
+    }
+    if (!number && word)
+    {
+        c++;
+    }
+    return c;
 }
 
-unsigned int faStr2(const char *str) {
-    return 0;
+unsigned int faStr2(const char *str)
+{
+    int c = 0;
+    bool word = false;
+    bool latin = false;
+    bool space = false;
+    bool t = true;
+    bool z = false;
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if (isspace(str[i]))
+        {
+            if (latin && word && t)
+            {
+                c++;
+            }
+            word = false;
+            latin = false;
+            space = true;
+        }
+        else
+        {
+            if (space)
+            {
+                space = false;
+                if (isalpha(str[i]))
+                {
+                    latin = true;
+                    t = true;
+                    if (isupper(str[i]))
+                    {
+                        word = true;
+                        t = true;
+                        z = true;
+                    }
+                    else
+                    {
+                        t = false;
+                    }
+                }
+                else
+                {
+                    t = false;
+                }
+            }
+            else
+            {
+                if (isalpha(str[i]))
+                {
+                    latin = true;
+                }
+                else
+                {
+                    latin = false;
+                }
+                if (isupper(str[i]))
+                {
+                    if (z)
+                    {
+                        word = false;
+                    }
+                    else
+                    {
+                        word = true;
+                        t = true;
+                        z = true;
+                    }
+                }
+            }
+        }
+    }
+    if (latin && word && t)
+    {
+        c++;
+    }
+    return c;
 }
 
-unsigned int faStr3(const char *str) {
-    return 0;
+unsigned int faStr3(const char *str)
+{
+    int c = 0;
+    int symbol = 0;
+    bool word = false;
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if (isspace(str[i]))
+        {
+            word = false;
+        }
+        else
+        {
+            if (!word)
+            {
+                word = true;
+                c++;
+            }
+            symbol++;
+        }
+    }
+    float lenght = static_cast<double>(symbol) / c;
+    return round(lenght);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
-// Copyright 2022 UNN-IASR
+// Kuznetsova Angelina 381907-3
+#include <iostream>
 #include "fun.h"
 
-int main() {
+int main()
+{
+    const char *str = "Hello, world!";
+    std::cout << faStr1(str) << std::endl;
+    std::cout << faStr2(str) << std::endl;
+    std::cout << faStr3(str) << std::endl;
     return 0;
 }
