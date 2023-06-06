637b17c7be24c5ba5a6c1d2a3b0d512524435182
LordlyThief <90777318+LordlyThief@users.noreply.github.com>
2023-06-06 22:58:23 +0300

    lab1 (11)

--- src/main.cpp
+++ src/main.cpp
@@ -9,5 +9,6 @@ int main() {
     std::cout << faStr1(str) << std::endl;
     std::cout << faStr2(str) << std::endl;
     std::cout << faStr3(str) << std::endl;
-    return 0; }
+    return 0; 
+    }
     
